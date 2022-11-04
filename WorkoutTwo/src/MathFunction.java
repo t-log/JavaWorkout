70ebc6ff696a7f24345da9091e7277d1f8821dee
akhil <akhil.paul@ymail.com>
2022-11-04 18:28:37 +0530

    Reduces lines of code for solution 3

--- WorkoutTwo/src/MathFunction.java
+++ WorkoutTwo/src/MathFunction.java
@@ -1,31 +1,18 @@
 import java.util.Scanner;
 
 public class MathFunction {
-    static int multiply(int a,int b){
-        return a*b;
+    static void multiply(int a,int b){
+        System.out.println(a*b);
     }
-    static float multiply(float a,float b){
-        return a*b;
+    static void multiply(float a,float b){
+        System.out.println(a*b);
     }
-    static float multiply(float a,int b){
-        return a*b;
+    static void multiply(float a,int b){
+        System.out.println(a*b);
     }
     public static void main(String[] args) {
-        int num1,num2;
-        float num3,num4;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter two integer numbers to multiply:");
-        num1 = sc.nextInt();
-        num2 = sc.nextInt();
-        System.out.println("Result:"+ multiply(num1,num2));
-        System.out.println("Enter two floating point numbers to multiply:");
-        num3 = sc.nextFloat();
-        num4 = sc.nextFloat();
-        System.out.println("Result:"+ multiply(num3,num4));
-        System.out.println("Enter int and float numbers to multiply:");
-        num1 = sc.nextInt();
-        num3 = sc.nextFloat();
-        System.out.println("Result:"+ multiply(num1,num3));
-
+        multiply(5,2);
+        multiply((float)10.5,(float)5.5);
+        multiply((float)5.5,10);
     }
 }
