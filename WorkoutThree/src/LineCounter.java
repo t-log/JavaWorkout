058993d564c70e66a8604e9d009652d29a5bd154
akhil <akhil.paul@ymail.com>
2022-11-22 00:52:28 +0530

    Adds solution 3

---
+++ WorkoutThree/src/LineCounter.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+    public class LineCounter {
+        public static void main(String[] args)
+        {
+            BufferedReader reader = null;
+            int vowelCount = 0;
+            int wordCount = 0;
+            int lineCount = 0;
+
+            try
+            {
+                reader = new BufferedReader(new FileReader("D:\\sampleOne.txt"));
+                String currentLine = reader.readLine();
+                while (currentLine != null)
+                {
+                    lineCount++;
+                    String[] words = currentLine.split(" ");
+                    wordCount = wordCount + words.length;
+
+                    for (int i=0 ; i<currentLine.length(); i++)
+                    {
+                        char ch = currentLine.charAt(i);
+                        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A'|| ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+                        {
+                            vowelCount++;
+                        }
+                    }
+
+                    currentLine = reader.readLine();
+                }
+                System.out.println("Number of vowels in file : "+ vowelCount);
+                System.out.println("Number of words in file : "+wordCount);
+                System.out.println("Number of lines in file : "+lineCount);
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            finally
+            {
+                try
+                {
+                    reader.close();
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
