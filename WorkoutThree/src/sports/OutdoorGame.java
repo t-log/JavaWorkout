4671bdb1fe734b4a624bfea31c16436fc720039a
akhil <akhil.paul@ymail.com>
2022-11-22 00:43:40 +0530

    Adds solution one

--- WorkoutThree/src/sports/OutdoorGame.java
+++ WorkoutThree/src/sports/OutdoorGame.java
@@ -1,11 +1,26 @@
 package sports;
 
-public class OutdoorGame {
-
+public abstract class OutdoorGame {
+    abstract void play();
 }
-class Football{
-
+class Football extends OutdoorGame{
+    void play()
+    {
+        System.out.println("Playing football");
+    }
 }
-class Cricket{
+class Cricket extends OutdoorGame{
+    void play()
+    {
+        System.out.println("Playing cricket");
+    }
+}
+class DriverClass{
+    public static void main(String[] args) {
+        Football f1=new Football();
+        f1.play();
 
+        Cricket c1=new Cricket();
+        c1.play();
+    }
 }
--- WorkoutThree/src/sportsstar/Footballer.java
+++ WorkoutThree/src/sportsstar/Footballer.java
@@ -1,4 +1,11 @@
 package sportsstar;
 
 public class Footballer {
+    int individualIncome;
+
+    void calculate_income(int rating)
+    {
+        this.individualIncome=rating*50000;
+        System.out.println("Individual income of footballer is "+this.individualIncome);
+    }
 }
